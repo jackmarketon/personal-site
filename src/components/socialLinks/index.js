71b9df816f1e56ff7cef28ac04369a2ac4bfa883
Jackson Marketon <jmarketon@gmail.com>
2021-08-08 20:56:35 -0700

    progress

--- src/components/socialLinks/index.js
+++ src/components/socialLinks/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Github, Twitter, Linkedin } from 'styled-icons/fa-brands';
+import { Github, Linkedin } from 'styled-icons/fa-brands';
 
 const Wrapper = styled.div`
   display: flex;
@@ -52,6 +52,5 @@ export default () => (
     >
       <Linkedin />
     </Link>
-    </ Test Master/Slave dirve />
   </Wrapper>
 );
