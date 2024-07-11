d798ddad58c6b4091b30772cdb29fa6df0d43afb
Jackson Marketon <jmarketon@gmail.com>
2024-07-10 20:39:56 -0700

    Adding social links

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Github from '~icons/mdi/github.jsx';
+import Linkedin from '~icons/mdi/linkedin.jsx';
 
 const Links = () => {
   return (
-    <div className="flex flex-col gap-2 text-gray-700 dark:text-grey-200">
+    <div className="flex flex-col gap-2 items-center text-gray-700 dark:text-grey-200">
       <h2 className="pb-4 text-xl font-bold">My links</h2>
-      <a className="text-md text-blue-600 visited:text-purple-600" href="https://www.linkedin.com/in/jacksonmarketon/" target="_blank">LinkedIn</a>
-      <a className="text-md text-blue-600 visited:text-purple-600" href="https://github.com/jackmarketon" target="_blank">GitHub</a>
+      <a className="text-md text-blue-600 visited:text-purple-600" href="https://www.linkedin.com/in/jacksonmarketon/" target="_blank"><Linkedin width={50} height={50}/></a>
+      <a className="text-md text-blue-600 visited:text-purple-600" href="https://github.com/jackmarketon" target="_blank"><Github width={50} height={50}/></a>
     </div>
   )
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+import Icons from "unplugin-icons/webpack";
+
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  webpack(config) {
+    config.plugins.push(
+      Icons({
+        compiler: "jsx",
+        jsx: "react",
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
